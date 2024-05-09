fe1c3153a8d45d8354a664dd6aacd767b1406d99
Akash Sahu <akashsahu793@gmail.com>
2024-05-09 18:53:29 +0530

    added reducer updateDirectConv and addDirectConv
    1 -> the error with the converstion -> redux store variable is still coming empty, and because of the no userList is showing in the "AllChats" tab
    2 -> multiple friend request is showing from the same account of a user to logged in user
    video - 31

--- src/redux/slices/conversation.js
+++ src/redux/slices/conversation.js
@@ -37,6 +37,53 @@ const slice = createSlice({
 
       state.direct_chat.conversations = list;
     },
+    updateDirectConversation(state, action) {
+      // data= {}
+      // list.map((el) => el.id === data._id ? data : el)
+
+      const this_conversation = action.payload.consversation;
+      state.direct_chat.conversations = state.direct_chat.conversations.map(
+        (el) => {
+          if (el.id !== this_conversation._id) {
+            return el;
+          } else {
+            const user = this_conversation.participats.find(
+              (elm) => elm._id.toString() !== user_id
+            );
+
+            return {
+              id: this.conversation._id, //conversation id
+              user_id: user?._id, // the person to whome we are chatting
+              name: `${user?.firstName} ${user?.lastName}`, // fullName of the person to whome we are chatting
+              online: user.status === "Online",
+              img: faker.image.avatar(),
+              msg: "mango",
+              time: "9:36",
+              unread: 0,
+              pinned: false,
+            };
+          }
+        }
+      );
+    },
+    addDirectConversation(state, action) {
+      // list.push(data)
+      const this_conversation = action.payload.consversation;
+      const user = this_conversation.participats.find(
+        (elm) => elm._id.toString() !== user_id
+      );
+      state.direct_chat.conversations.push({
+        id: this_conversation._id, //conversation id
+        user_id: user?._id, // the person to whome we are chatting
+        name: `${user?.firstName} ${user?.lastName}`, // fullName of the person to whome we are chatting
+        online: user.status === "Online",
+        img: faker.image.avatar(),
+        msg: "banana",
+        time: "9:36",
+        unread: 0,
+        pinned: false,
+      });
+    },
   },
 });
 
@@ -48,5 +95,15 @@ export default slice.reducer;
 export const FetchDirectConversation = ({ conversations }) => {
   return async (dispatch, getState) => {
     dispatch(slice.actions.fetchDirectConversations({ conversations }));
+  };
+};
+export const AddDirectConversation = ({ conversation }) => {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.addDirectConversation({ conversation }));
+  };
+};
+export const UpdateDirectConversation = ({ conversation }) => {
+  return async (dispatch, getState) => {
+    dispatch(slice.actions.updateDirectConversation({ conversation }));
   };
 };
